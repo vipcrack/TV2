2fb87fc55fac6d6ea67b358ca2760ed3bff6b26c
vipcrack <bcgcrack@163.com>
2025-12-06 11:58:30 +0800

    Delete app directory

--- app/src/main/java/com/ffzy/tv/ui/PlayerScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.ffzy.tv.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun PlayerScreen(videoUri: String) {
-    // 播放器实现略（需集成 ExoPlayer）
-    androidx.compose.foundation.layout.Box()
-}
